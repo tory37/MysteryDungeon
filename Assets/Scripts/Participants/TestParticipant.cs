aef21ce7fd31219ef5a58c2dc4c95536beb20128
tory37 <tory37@gmail.com>
2016-03-16 19:18:33 -0500

    added missing stuff

---
+++ Assets/Scripts/Participants/TestParticipant.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestParticipant : Participant {
+
+	public override TurnType DetermineTurn()
+	{
+		return TurnType.Move;
+	}
+
+	public override void TakeAction( FinishedActionCallback callback )
+	{
+		callback();
+	}
+
+	public override Cell FindNewCell()
+	{
+		//Cell newCell = new Cell(Column, Row);
+
+		//for ( int i = 0; i < 50; i++ )
+		//{
+		//	int rC = Random.Range( -1, 1 );
+		//	int rR = Random.Range( -1, 1 );
+
+		//	if ( (rC != 0 || rR != 0) && LevelManager.Instance.FloorCells[rC + Column, rR + Row] == CellStatus.UnOccupied )
+		//	{
+		//		newCell = new Cell( rC + Column, rR + Row );
+		//		break;
+		//	}
+		//}
+
+		return new Cell(Column + Random.Range(-1, 1), Row + Random.Range(-1, 1));
+	}
+}
