688b642013c1a59be4a680e73f7e7a06c511280b
toryh <toryh@lafayette.org>
2016-03-29 09:46:44 -0500

    tried something new with

--- Assets/Scripts/LevelManager/LM_MoveParticipants.cs
+++ Assets/Scripts/LevelManager/LM_MoveParticipants.cs
@@ -7,7 +7,9 @@ public class LM_MoveParticipants : State
 	#region Editor Interface
 
 	[SerializeField]
-	private float eMoveSpeed;
+	private float moveTime;
+	[SerializeField]
+	private float moveIntervals;
 
 	#endregion
 
@@ -15,15 +17,14 @@ public class LM_MoveParticipants : State
 
 	private LevelManager fsm;
 
-	/// <summary>
-	/// The distance the characters have moved so far
-	/// </summary>
-	private float deltaMove;
+	private float moveIntervalTime;
 
-	private float moveSpeed;
+	private float intervalFraction;
 
 	// A list of the directions each character needs to move
 	private List<Vector3> directions;
+
+	private bool done;
 
 	#endregion
 
@@ -34,15 +35,16 @@ public class LM_MoveParticipants : State
 		fsm = (LevelManager)callingfsm;
 
 		// TODO: Make this use a Game Manager
-		moveSpeed = eMoveSpeed;
+		moveIntervalTime = moveTime / moveIntervals;
+		intervalFraction = 1 / moveIntervals;
 	}
 
 	public override void OnEnter()
 	{
-		foreach ( Participant p in LevelManager.Instance.ParticipantsToMove )
-			Debug.Log( p.transform.position + ", " + p.Column + ", " + p.Row );
+		done = false;
 
-		deltaMove = 0;
+		//foreach ( Participant p in LevelManager.Instance.ParticipantsToMove )
+		//	Debug.Log( p.transform.position + ", " + p.Column + ", " + p.Row );
 
 		directions = new List<Vector3>();
 		// Set the directions, we assume the Column and Row of the participant has been set to the target location
@@ -53,27 +55,20 @@ public class LM_MoveParticipants : State
 		}
 	}
 
-	public override void OnUpdate()
-	{
-		for (int i = 0; i < fsm.ParticipantsToMove.Count; i++)
-		{
-			Participant p = fsm.ParticipantsToMove[i];
-			p.transform.Translate(directions[i] * moveSpeed * Time.deltaTime);
-		}
-        deltaMove += moveSpeed * Time.deltaTime;
-	}
+	//public override void OnUpdate()
+	//{
+	//	for (int i = 0; i < fsm.ParticipantsToMove.Count; i++)
+	//	{
+	//		Participant p = fsm.ParticipantsToMove[i];
+	//		p.transform.Translate(directions[i] * moveSpeed * Time.deltaTime);
+	//	}
+	//	deltaMove += moveSpeed * Time.deltaTime;
+	//}
 
 	public override void CheckTransitions()
 	{
-		if (deltaMove >= 1)
+		if (done)
 		{
-			// Lock them into place, accounting for any small error
-			for (int i = 0; i < fsm.ParticipantsToMove.Count; i++)
-			{
-				Participant p = fsm.ParticipantsToMove[i];
-				p.transform.position = new Vector3( p.Column, 0f, p.Row );
-			}
-
 			// Signify everyones been moved
 			fsm.FinishedMovingParticipants();
 
@@ -82,4 +77,20 @@ public class LM_MoveParticipants : State
 	}
 
 	#endregion
+
+	private IEnumerator Move()
+	{
+		for (int i = 0; i < moveIntervals; i++)
+		{
+			for (int par = 0; i < fsm.ParticipantsToMove.Count; par++)
+			{
+				Participant p = fsm.ParticipantsToMove[par];
+				p.transform.Translate( directions[par] * intervalFraction );
+			}
+
+			yield return new WaitForSeconds( moveIntervalTime );
+		}
+
+		done = true;
+	}
 }
